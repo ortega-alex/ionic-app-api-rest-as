5c4cb19def6db90fceb9e238a06d882bc4423bee
alex <alex@alex-Mac-mini-2.local>
2018-06-05 23:26:08 -0600

    version 1.0.0 ajustes en popover dinamico y vista de animacion en madal

--- src/pages/popover/popover.ts
+++ src/pages/popover/popover.ts
@@ -11,6 +11,7 @@ export class PopoverPage {
 
   private title: string;
   private informacion: string;
+  private posicion : number;
 
   constructor(
     public navCtrl: NavController,
@@ -18,8 +19,14 @@ export class PopoverPage {
     private globalProvider: GlobalProvider,
     private viewController: ViewController
   ) {
-    this.informacion = this.globalProvider.data.info[this.navParams.get('posicion')].msj;
-    this.title = this.globalProvider.data.info[this.navParams.get('posicion')].title;
+    this.posicion = this.navParams.get('posicion');
+    this.title = this.globalProvider.data.info[this.posicion].title;
+    if(this.posicion == 0){
+      let i = this.globalProvider.data.info[this.posicion].msj;
+      this.informacion = i.split('#');      
+    } else {
+      this.informacion = this.globalProvider.data.info[this.posicion].msj;
+    }
   }
 
   ionViewDidLoad() { }
